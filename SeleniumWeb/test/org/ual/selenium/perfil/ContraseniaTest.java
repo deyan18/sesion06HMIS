3692c1c3b05c355ba0f0fa3d44fca95219c8d478
Maria Galvez Gonzalez <mgg694@inlumine.ual.es>
2021-04-12 21:23:16 +0200

    Test correctos

--- SeleniumWeb/test/org/ual/selenium/perfil/ContraseniaTest.java
+++ SeleniumWeb/test/org/ual/selenium/perfil/ContraseniaTest.java
@@ -17,6 +17,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.JavascriptExecutor;
@@ -26,7 +27,7 @@ import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.net.MalformedURLException;
 import java.net.URL;
-public class PerfilTest {
+public class ContraseniaTest {
   private WebDriver driver;
   private Map<String, Object> vars;
   JavascriptExecutor js;
@@ -73,8 +74,8 @@ public class PerfilTest {
     driver.quit();
   }
   @Test
-  public void perfilcorrecto() {
-    // Test name: Perfil_correcto
+  public void contraseniadistinta() {
+    // Test name: Contrasenia_distinta
     // Step # | name | target | value
     // 1 | open | http://hmis21-weblogin2.azurewebsites.net/login | 
     driver.get("http://hmis21-weblogin2.azurewebsites.net/login");
@@ -112,29 +113,26 @@ public class PerfilTest {
     driver.findElement(By.id("header-account-menu-link")).click();
     // 16 | click | linkText=Settings | 
     driver.findElement(By.linkText("Settings")).click();
-    // 17 | click | linkText=Edit profile | 
-    driver.findElement(By.linkText("Edit profile")).click();
-    // 18 | click | id=full-name | 
-    driver.findElement(By.id("full-name")).click();
-    // 19 | type | id=full-name | nombre
-    driver.findElement(By.id("full-name")).sendKeys(Keys.CONTROL, "a", Keys.DELETE);
-    driver.findElement(By.id("full-name")).sendKeys("nombre");
-    // 20 | click | css=.button-text | 
+    // 17 | click | linkText=Change password | 
+    driver.findElement(By.linkText("Change password")).click();
+    // 18 | type | id=password | 123
+    driver.findElement(By.id("password")).sendKeys("123");
+    // 19 | click | id=confirm-password | 
+    driver.findElement(By.id("confirm-password")).click();
+    // 20 | type | id=confirm-password | 1234
+    driver.findElement(By.id("confirm-password")).sendKeys("1234");
+    // 21 | click | css=.button-text | 
     driver.findElement(By.cssSelector(".button-text")).click();
-    // 21 | assertText | css=.row:nth-child(4) strong | nombre
-    {
-    	WebDriverWait wait = new WebDriverWait(driver, 20);
-    	wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Edit profile")));
-    }
-    assertThat(driver.findElement(By.cssSelector(".row:nth-child(4) strong")).getText(), is("nombre"));
-    // 22 | click | id=header-account-menu-link | 
+    // 22 | assertText | css=.invalid-feedback | Your new password and confirmation do not match.
+    assertThat(driver.findElement(By.cssSelector(".invalid-feedback")).getText(), is("Your new password and confirmation do not match."));
+    // 23 | click | id=header-account-menu-link | 
     driver.findElement(By.id("header-account-menu-link")).click();
-    // 23 | click | linkText=Sign out | 
+    // 24 | click | linkText=Sign out | 
     driver.findElement(By.linkText("Sign out")).click();
   }
   @Test
-  public void perfilcorreoincompleto1() {
-    // Test name: Perfil_correo_incompleto1
+  public void contraseniauncampo() {
+    // Test name: Contrasenia_un_campo
     // Step # | name | target | value
     // 1 | open | http://hmis21-weblogin2.azurewebsites.net/login | 
     driver.get("http://hmis21-weblogin2.azurewebsites.net/login");
@@ -172,25 +170,22 @@ public class PerfilTest {
     driver.findElement(By.id("header-account-menu-link")).click();
     // 16 | click | linkText=Settings | 
     driver.findElement(By.linkText("Settings")).click();
-    // 17 | click | linkText=Edit profile | 
-    driver.findElement(By.linkText("Edit profile")).click();
-    // 18 | click | id=email-address | 
-    driver.findElement(By.id("email-address")).click();
-    // 19 | type | id=email-address | ual@a
-    driver.findElement(By.id("email-address")).sendKeys(Keys.CONTROL, "a", Keys.DELETE);
-    driver.findElement(By.id("email-address")).sendKeys("ual@a");
-    // 20 | click | css=.button-text | 
+    // 17 | click | linkText=Change password | 
+    driver.findElement(By.linkText("Change password")).click();
+    // 18 | type | id=password | 123
+    driver.findElement(By.id("password")).sendKeys("123");
+    // 19 | click | css=.button-text | 
     driver.findElement(By.cssSelector(".button-text")).click();
-    // 21 | assertText | css=.invalid-feedback | Please enter a valid email address.
-    assertThat(driver.findElement(By.cssSelector(".invalid-feedback")).getText(), is("Please enter a valid email address."));
-    // 22 | click | id=header-account-menu-link | 
+    // 20 | assertText | css=.invalid-feedback | Your new password and confirmation do not match.
+    assertThat(driver.findElement(By.cssSelector(".invalid-feedback")).getText(), is("Your new password and confirmation do not match."));
+    // 21 | click | id=header-account-menu-link | 
     driver.findElement(By.id("header-account-menu-link")).click();
-    // 23 | click | linkText=Sign out | 
+    // 22 | click | linkText=Sign out | 
     driver.findElement(By.linkText("Sign out")).click();
   }
   @Test
-  public void perfilcorreoincompleto2() {
-    // Test name: Perfil_correo_incompleto2
+  public void contraseniavacia() {
+    // Test name: Contrasenia_vacia
     // Step # | name | target | value
     // 1 | open | http://hmis21-weblogin2.azurewebsites.net/login | 
     driver.get("http://hmis21-weblogin2.azurewebsites.net/login");
@@ -228,84 +223,17 @@ public class PerfilTest {
     driver.findElement(By.id("header-account-menu-link")).click();
     // 16 | click | linkText=Settings | 
     driver.findElement(By.linkText("Settings")).click();
-    // 17 | click | linkText=Edit profile | 
-    driver.findElement(By.linkText("Edit profile")).click();
-    // 18 | click | id=email-address | 
-    driver.findElement(By.id("email-address")).click();
-    // 19 | type | id=email-address | ual@algo.
-    driver.findElement(By.id("email-address")).sendKeys(Keys.CONTROL, "a", Keys.DELETE);
-    driver.findElement(By.id("email-address")).sendKeys("ual@algo.");
-    // 20 | click | css=.button-text | 
+    // 17 | click | linkText=Change password | 
+    driver.findElement(By.linkText("Change password")).click();
+    // 18 | click | css=.button-text | 
     driver.findElement(By.cssSelector(".button-text")).click();
-    // 21 | executeScript | return document.getElementById("email-address").validationMessage | message
-    vars.put("message", js.executeScript("return document.getElementById(\"email-address\").validationMessage"));
-    // 22 | echo | ${message} | 
-    System.out.println(vars.get("message").toString());
-    // 23 | assert | message | El signo "." está colocado en una posición incorrecta en la dirección "algo.".
-    assertEquals(vars.get("message").toString(), "El signo \".\" está colocado en una posición incorrecta en la dirección \"algo.\".");
-    // 24 | click | id=header-account-menu-link | 
+    // 19 | assertText | css=.col-sm-6:nth-child(1) .invalid-feedback | Please enter a password or choose "Cancel".
+    assertThat(driver.findElement(By.cssSelector(".col-sm-6:nth-child(1) .invalid-feedback")).getText(), is("Please enter a password or choose \"Cancel\"."));
+    // 20 | assertText | css=.col-sm-6:nth-child(2) .invalid-feedback | Your new password and confirmation do not match.
+    assertThat(driver.findElement(By.cssSelector(".col-sm-6:nth-child(2) .invalid-feedback")).getText(), is("Your new password and confirmation do not match."));
+    // 21 | click | id=header-account-menu-link | 
     driver.findElement(By.id("header-account-menu-link")).click();
-    // 25 | click | linkText=Sign out | 
-    driver.findElement(By.linkText("Sign out")).click();
-  }
-  @Test
-  public void perfilcorreosinarroba() {
-    // Test name: Perfil_correo_sin_arroba
-    // Step # | name | target | value
-    // 1 | open | http://hmis21-weblogin2.azurewebsites.net/login | 
-    driver.get("http://hmis21-weblogin2.azurewebsites.net/login");
-    // 2 | setWindowSize | 1246x824 | 
-    driver.manage().window().setSize(new Dimension(1246, 824));
-    // 3 | click | linkText=Sign up | 
-    driver.findElement(By.linkText("Sign up")).click();
-    // 4 | executeScript | return "ual-" + Math.floor(Math.random()*1500000) + "@ual.es" | emailrandom
-    vars.put("emailrandom", js.executeScript("return \"ual-\" + Math.floor(Math.random()*1500000) + \"@ual.es\""));
-    // 5 | echo | ${emailrandom} | 
-    System.out.println(vars.get("emailrandom").toString());
-    // 6 | type | id=full-name | Nombre
-    driver.findElement(By.id("full-name")).sendKeys("Nombre");
-    // 7 | click | id=email-address | 
-    driver.findElement(By.id("email-address")).click();
-    // 8 | type | id=email-address | ${emailrandom}
-    driver.findElement(By.id("email-address")).sendKeys(vars.get("emailrandom").toString());
-    // 9 | click | id=password | 
-    driver.findElement(By.id("password")).click();
-    // 10 | type | id=password | 123456
-    driver.findElement(By.id("password")).sendKeys("123456");
-    // 11 | click | id=confirm-password | 
-    driver.findElement(By.id("confirm-password")).click();
-    // 12 | type | id=confirm-password | 123456
-    driver.findElement(By.id("confirm-password")).sendKeys("123456");
-    // 13 | click | id=terms-agreement | 
-    driver.findElement(By.id("terms-agreement")).click();
-    // 14 | click | css=.button-text | 
-    driver.findElement(By.cssSelector(".button-text")).click();
-    // 15 | click | id=header-account-menu-link | 
-    {
-    	WebDriverWait wait = new WebDriverWait(driver, 20);
-    	wait.until(ExpectedConditions.elementToBeClickable(By.id("header-account-menu-link")));
-    }
-    driver.findElement(By.id("header-account-menu-link")).click();
-    // 16 | click | linkText=Settings | 
-    driver.findElement(By.linkText("Settings")).click();
-    // 17 | click | linkText=Edit profile | 
-    driver.findElement(By.linkText("Edit profile")).click();
-    // 18 | click | id=email-address | 
-    driver.findElement(By.id("email-address")).click();
-    // 19 | type | id=email-address | ual
-    driver.findElement(By.id("email-address")).sendKeys(Keys.CONTROL, "a", Keys.DELETE);
-    driver.findElement(By.id("email-address")).sendKeys("ual");
-    // 20 | click | css=.button-text | 
-    driver.findElement(By.cssSelector(".button-text")).click();
-    // 21 | executeScript | return document.getElementById("email-address").validationMessage | message
-    vars.put("message", js.executeScript("return document.getElementById(\"email-address\").validationMessage"));
-    // 22 | echo | ${message} | 
-    System.out.println(vars.get("message").toString());
-    // 23 | assert | message | Incluye un signo "@" en la dirección de correo electrónico. La dirección "ual" no incluye el signo "@".
-    assertEquals(vars.get("message").toString(), "Incluye un signo \"@\" en la dirección de correo electrónico. La dirección \"ual\" no incluye el signo \"@\".");
-    // 24 | click | id=header-account-menu-link | 
-    driver.findElement(By.id("header-account-menu-link")).click();
-    // 25 | click | linkText=Sign out | 
+    // 22 | click | linkText=Sign out | 
     driver.findElement(By.linkText("Sign out")).click();
   }
 }
